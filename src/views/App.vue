b6275ab2f31400aa8e483f48e4ddf0ba2d3d6649
覃超成 <meimaonong@163.com>
2017-11-05 22:08:46 +0800

    boss提交

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <sysHeader></sysHeader>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import sysHeader from './layout/header/header.vue'
+
 export default {
   name: 'app',
   data () {
@@ -14,30 +17,32 @@ export default {
   },
   mounted() {
 
-    Vue.http.post("http://www.easy-mock.com/mock/5996a438059b9c566dc6de34/example/article/list", {}).then((response) => {
-        console.log(response.body)
-    })
+    
 
-    // axios.post('http://www.easy-mock.com/mock/5996a438059b9c566dc6de34/example/article/list')
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-
+  },
+  components: {
+    sysHeader
   }
 }
 </script>
 
 <style lang="less">
+body {
+    font-size: 14px;
+    background: #f0f0f0;
+    margin: 0;
+}
+
+a {
+    color: #4078c0;
+    text-decoration: none;
+}
+
 #app {
-  font-family: Microsoft Yahei, 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Microsoft Yahei', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
 }
 
 </style>
